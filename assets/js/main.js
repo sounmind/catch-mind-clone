25f6aeb45e218b1f96df7c7a7176473e8ab5ac95
sounmind <sounmind777@gmail.com>
2021-03-01 12:44:12 +0900

    feat(login): input nickname & set localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
-import { something } from "./chat";
+const $body = document.querySelector("body");
+const nickname = localStorage.getItem("nickname");
+const LOGGED_OUT = "loggedOut";
+const LOGGED_IN = "loggedIn";
+const NICKNAME = "nickname";
+const $loginForm = document.querySelector("#jsLogin");
 
-console.log("hello", something);
+if (!nickname) {
+  $body.className = LOGGED_OUT;
+} else {
+  $body.className = LOGGED_IN;
+}
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+  const input = $loginForm.querySelector("input");
+  const { value } = input;
+  input.value = ""; // reset
+  localStorage.setItem(NICKNAME, value);
+};
+
+if ($loginForm) {
+  $loginForm.addEventListener("submit", handleFormSubmit);
+}
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,7 +1,4 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.something=void 0;var something="test";exports.something=something;
+"use strict";var $body=document.querySelector("body"),nickname=localStorage.getItem("nickname"),LOGGED_OUT="loggedOut",LOGGED_IN="loggedIn",NICKNAME="nickname",$loginForm=document.querySelector("#jsLogin");$body.className=nickname?LOGGED_IN:LOGGED_OUT;var handleFormSubmit=function(e){e.preventDefault();var o=$loginForm.querySelector("input"),n=o.value;o.value="",localStorage.setItem(NICKNAME,n)};$loginForm&&$loginForm.addEventListener("submit",handleFormSubmit);
 
-},{}],2:[function(require,module,exports){
-"use strict";var _chat=require("./chat");console.log("hello",_chat.something);
-
-},{"./chat":1}]},{},[2]);
+},{}]},{},[1]);
